a7f59bcdd8b191ef23a752563864b6d44874a3e4
fahlanlogic <alanfahdi@gmail.com>
2024-02-02 22:30:17 +0700

    complete array random, split array into odd and even, min, max, total, avarage

--- index.js
+++ index.js
@@ -5,68 +5,60 @@ for (i = 0; i < 100; i++) {
 }
 
 // Pecahlah menjadi 2 array berdasarkan indexnya, yakni array pada index genap dan array pada index ganjil
-const oddArray = [];
 const evenArray = [];
+const oddArray = [];
 
-for (let i = 0; i < myArray; i++) {
-  if (myArray[i] % 2 === 0) {
-    evenArray.push(myArray[i])
-  } else {
-    oddArray.push(myArray[i])
-  };
+// Pastikan masing-masing bilangan length-nya 50
+while (evenArray.length < 50) {
+	let randomValue = Math.floor(Math.random() * 51);
+	if (randomValue % 2 === 0) {
+		evenArray.push(randomValue);
+	}
 }
 
-console.log(oddArray.length);
-console.log(evenArray.length);
-
-// Mencari bilangan Min pada array ganjil/oddArray
-let minOddArray = oddArray[0];
-for (i = 0; i < oddArray.length; i++) {
-  if (oddArray[i] < minOddArray) {
-    minOddArray = oddArray[i]
-  };
+while (oddArray.length < 50) {
+	let randomValue = Math.floor(Math.random() * 51);
+	if (randomValue % 2 !== 0) {
+		oddArray.push(randomValue);
+	}
 }
 
-// Mencari bilangan Max pada array ganjil/oddArray
-let maxOddArray = oddArray[0];
-for (i = 0; i < oddArray.length; i++) {
-  if (oddArray[i] > maxOddArray) {
-    maxOddArray = oddArray[i]
-  };
-}
-
-// Mencari bilangan Min pada array genap/evenArray
+// Mencari bilangan Min & Max pada array genap/evenArray
 let minEvenArray = evenArray[0];
-for (i = 0; i < evenArray.length; i++) {
-  if (evenArray[i] < minEvenArray) {
-    minEvenArray = evenArray[i]
-  };
-}
-
-// Mencari bilangan Max pada array genap/evenArray
 let maxEvenArray = evenArray[0];
 for (i = 0; i < evenArray.length; i++) {
-  if (evenArray[i] > maxEvenArray) {
-    maxEvenArray = evenArray[i]
-  };
+	if (evenArray[i] < minEvenArray) {
+		minEvenArray = evenArray[i];
+	} else if (evenArray[i] > maxEvenArray) {
+		maxEvenArray = evenArray[i];
+	}
 }
 
-// Menghitung Total pada array ganjil/oddArray
-let totalOddArray = 0;
-for (i = 0; i < oddArray.length; i++) {
-  totalOddArray += oddArray[i];
+// Mencari bilangan Min & Max pada array ganjil/oddArray
+let minOddArray = oddArray[0];
+let maxOddArray = oddArray[0];
+for (let i = 0; i < oddArray.length; i++) {
+	if (oddArray[i] < minOddArray) {
+		minOddArray = oddArray[i];
+	} else if (oddArray[i] > maxOddArray) {
+		maxOddArray = oddArray[i];
+	}
 }
 
 // Menghitung Total pada array genap/evenArray
 let totalEvenArray = 0;
 for (i = 0; i < evenArray.length; i++) {
-  totalEvenArray += evenArray[i];
+	totalEvenArray += evenArray[i];
+}
+
+// Menghitung Total pada array ganjil/oddArray
+let totalOddArray = 0;
+for (i = 0; i < oddArray.length; i++) {
+	totalOddArray += oddArray[i];
 }
 
 // Menghitung Rata-rata pada array ganjil/oddArray
-// let averageOddValue = oddArray[i] / 100; 
+let averageEvenValue = Math.floor(totalEvenArray / evenArray.length);
 
-// console.log(totalEvenArray);
-// console.log(totalOddArray);
-// console.log(oddArray.length);
-// console.log(evenArray.length);
+// Menghitung Rata-rata pada array ganjil/oddArray
+let averageOddValue = Math.floor(totalOddArray / oddArray.length);
